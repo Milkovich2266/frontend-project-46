a55a03a47277e1172dc09965c05619bfc28344d1
Andrey Gorbunov <milkovich2266@gmail.com>
2023-11-17 01:06:40 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-const { program } = import('commander');
+import { program } from 'commander';
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+
   
 
   program.parse();
