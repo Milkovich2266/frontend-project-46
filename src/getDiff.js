9b364736fa87997ca18d3d1d67ba4578f2591c43
Andrey Gorbunov <milkovich2266@gmail.com>
2024-01-02 17:08:58 +0300

    update getDiff.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,8 +1,10 @@
 const getDiff = (filepath1, filepath2) => {
     const сurrentPath = process.cwd();
-    const pathFile1 = path.resolve(сurrentPath, filepath1);
+    const pathFile1 = path.resolve(сurrentPath, '/__fixtures__', filepath1);
     const pathFile2 = path.resolve(сurrentPath, '/__fixtures__', filepath2);
     console.log(pathFile1);
-};//`${filepath1}, ${filepath2}`;
+    /*const readFile1 = readFileSync(pathFile1);
+    const readFile2 = readFileSync(pathFile2);*/
+}; 
 
 export default getDiff; 
