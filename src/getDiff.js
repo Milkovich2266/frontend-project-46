0f86748df47914a7a32ed5124af7b600bf5e725b
Andrey Gorbunov <milkovich2266@gmail.com>
2024-01-04 02:57:56 +0300

    remove gitignore files

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,10 +1,14 @@
+import * as path from 'path';
+import { readFileSync } from 'fs';
+import parsingFiles from '../bin/dataParsing.js'
+
 const getDiff = (filepath1, filepath2) => {
     const сurrentPath = process.cwd();
-    const pathFile1 = path.resolve(сurrentPath, '/__fixtures__', filepath1);
-    const pathFile2 = path.resolve(сurrentPath, '/__fixtures__', filepath2);
-    console.log(pathFile1);
-    /*const readFile1 = readFileSync(pathFile1);
-    const readFile2 = readFileSync(pathFile2);*/
+    const pathFile1 = path.resolve(сurrentPath, filepath1);
+    const pathFile2 = path.resolve(сurrentPath, filepath2);
+    const readFile1 = readFileSync(pathFile1, 'utf8');
+    const readFile2 = readFileSync(pathFile2, 'utf8');
+    parsingFiles(readFile1, readFile2); 
 }; 
 
 export default getDiff; 
