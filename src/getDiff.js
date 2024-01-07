8a856c359553c2031f012a0d437f48b02bedfeff
Andrey Gorbunov <milkovich2266@gmail.com>
2024-01-08 01:18:53 +0300

    add func  getDeepEqual

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,6 +1,32 @@
 import * as path from 'path';
 import { readFileSync } from 'fs';
 import parsingFiles from '../src/dataParsing.js'
+import _ from 'lodash';
+
+const getDeepEqual = (file1, file2) => {
+    const arrayFile1 = Object.keys(file1);
+    const arrayFile2 = Object.keys(file2);
+    const combineArrays = _.union(arrayFile1, arrayFile2);
+    const turnOverCombiArrays = _.sortBy(combineArrays)
+    
+    let acc = '';
+    (() => { turnOverCombiArrays.map((key) => {
+      if (arrayFile2.includes(key)) {
+        if (_.isEqual(file2[key], file1[key])) {
+          acc += `    ${key}: ${file1[key]}\n`;
+        } else if (arrayFile2.includes(key) && !arrayFile1.includes(key)) {
+          acc += `  + ${key}: ${file2[key]}\n`;
+        }
+        else {
+          acc += `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
+        }   
+      } else {
+        acc += `  - ${key}: ${file1[key]}\n`;
+      }
+    });
+  })();
+    console.log(`{\n${acc}}`);
+  };
 
 const getDiff = (filepath1, filepath2) => {
     const typeFile1 = path.extname(filepath1).slice(1);
@@ -12,11 +38,10 @@ const getDiff = (filepath1, filepath2) => {
     const readFile1 = readFileSync(pathFile1, 'utf8');
     const readFile2 = readFileSync(pathFile2, 'utf8');
 
-    parsingFiles(readFile1, typeFile1); 
-    parsingFiles(readFile2, typeFile2);
-
+    const parsingFil1 = parsingFiles(readFile1, typeFile1); 
+    const parsingFil2 = parsingFiles(readFile2, typeFile2);
+    getDeepEqual(parsingFil1, parsingFil2);
 }; 
-
 
 
 export default getDiff; 
