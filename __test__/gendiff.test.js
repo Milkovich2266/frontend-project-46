394bae02d7cb56b828734714d17ec052ce7bf35b
Andrey Gorbunov <milkovich2266@gmail.com>
2024-03-08 20:21:06 +0300

    upgrade gendiff.test.js

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -7,13 +7,12 @@ const __dirname = dirname(__filename);
 
 
 test ('genDiff', () => {
-    expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(`{
-      - follow: false
+    expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
+      `- follow: false
       host: hexlet.io
       - proxy: 123.234.53.22
       - timeout: 50
       + timeout: 20
-      + verbose: true
-    }`);
+      + verbose: true`);
 });
 
