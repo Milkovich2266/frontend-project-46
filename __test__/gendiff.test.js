9f685dd557f3e8f2936a8cb834f60abb37b94266
Andrey Gorbunov <milkovich2266@gmail.com>
2024-03-15 01:38:48 +0300

    update gendiff.test.js

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,18 +1,16 @@
 import getDiff from '../src/getDiff.js';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, path } from 'path';
+import { test, expect } from '@jest/globals';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 
 test ('genDiff', () => {
-    expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
-      `- follow: false
-      host: hexlet.io
-      - proxy: 123.234.53.22
-      - timeout: 50
-      + timeout: 20
-      + verbose: true`);
+    expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(getFixturePath('expected_file.txt'));
 });
 
