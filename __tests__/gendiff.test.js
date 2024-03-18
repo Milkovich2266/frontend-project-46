82cc0bd90f75c03d96e47158ee2f91e840847c7f
Andrey Gorbunov <milkovich2266@gmail.com>
2024-03-18 12:58:36 +0300

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,11 +1,11 @@
 import getDiff from '../src/getDiff.js';
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import path from 'path';
 import { test, expect } from '@jest/globals';
 
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
