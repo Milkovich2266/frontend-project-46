ef8c2737f23e7369de0abb4ca2f746d435473301
Andrey Gorbunov <milkovich2266@gmail.com>
2024-03-19 01:24:52 +0300

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,12 +11,12 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-const readfile1 = readFixture('file1.json');
-const readFile2 = readFixture('file2.json');
+const getPathFile1 = getFixturePath('file1.json');
+const getPathFile2 = getFixturePath('file2.json');
 const readExpectedFile = readFixture('expected_file.txt');
 
 
 test ('genDiff', () => {
-    expect(getDiff(readfile1, readFile2)).toEqual(readExpectedFile);
+    expect(getDiff(getPathFile1, getPathFile2)).toEqual(readExpectedFile);
 });
 
