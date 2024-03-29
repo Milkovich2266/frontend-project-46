09fcd2d08e2db463ef2efd0f858a14a509fbd9d4
Andrey Gorbunov <milkovich2266@gmail.com>
2024-03-30 02:52:21 +0300

    add file.yml

--- src/dataParsing.js
+++ src/dataParsing.js
@@ -1,7 +1,13 @@
 
+import yaml from 'js-yaml';
+
 const parsingFiles = (file, typeFiles) => {
     if (typeFiles == 'json') { 
       const objFile = JSON.parse(file);
+      return objFile;
+    }
+    if (typeFiles == 'yml' || typeFiles == 'yaml') {
+      const objFile = yaml.load(file);
       return objFile;
     }
     console.log('no');
